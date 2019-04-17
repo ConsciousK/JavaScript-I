685c5565a23ef399041cb45f8f82b006b309ba60
Kevin Grant <ConsciousK@yahoo.com>
2019-04-17 19:10:22 -0400

    initial commit. Completed one function to arrow function conversion

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -15,10 +15,9 @@
 // };
 // add(1,2);
 
-// let subtract = function (param1, param2) {
-//   return param1 - param2;
-// };
-// subtract(1,2);
+let subtract =(param1, param2) => param1 - param2;
+
+subtract(1,2);
 
 
 // Stretch
